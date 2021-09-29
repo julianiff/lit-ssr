8a1bfab01413ef1cd0da15b442d3de4f2660bc77
julianiff <julian.iff@helsana.ch>
2021-09-29 17:32:55 +0200

    feat: update to lit-2.0 and lit-labs 1.0

--- src/template.ts
+++ src/template.ts
@@ -1,30 +1,25 @@
-import { LitElement, html, css } from 'lit';
-import {property, customElement } from 'lit/decorators.js'
+// @ts-ignore
+import { LitElement, html } from "lit";
+import { customElement } from "lit/decorators.js";
 
-@customElement('test-wc')
-class MyComponent extends LitElement {
+@customElement("test-wc")
+export class MyComponent extends LitElement {
+  //     private static styles = css`
+  //         :host {
+  //             color: var(--my-element-text-color, yellow);
+  //             background: var(--my-element-background-color, green);
+  //             font-family: var(--my-element-font-family, Roboto);
+  //             display: block;
+  //             padding: 8px;
+  //             margin: 8px;
+  //         }
+  //   `;
 
-    static styles = css`
-        :host {
-            color: var(--my-element-text-color, yellow);
-            background: var(--my-element-background-color, green);
-            font-family: var(--my-element-font-family, Roboto);
-            display: block;
-            padding: 8px;
-            margin: 8px;
-        }
-  `;
-
-
-
-    render () {
-        return html`<div><slot></slot></div> `
-    }
+  override render() {
+    return html`<div><slot></slot></div> `;
+  }
 }
-
 
 export const myTemplate = (value: string) => {
-    return html`
-        <test-wc>${value}</test-wc>
-    `
-}
+  return html` <test-wc><div>${value}</div></test-wc> `;
+};
